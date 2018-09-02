2d6b60e7c4c6e27cf1e88ce67f160843dfc372c6
Henrique <h.macedo@hotmail.com>
2018-09-02 13:43:48 -0300

    Atualizado às 13:43 hs de 02/09/2018

--- impressLista.py
+++ impressLista.py
@@ -1,5 +1,6 @@
 jogadores = [{'nome': 'Nelson', 'gols': [0, 2, 1], 'total': 3}, {'nome': 'Mario', 'gols': [2, 1, 3], 'total': 3}]
 tot = 0
+idx = 1
 
 print('-'*35)
 print(f'{"Cod":<5}{"Nome":<10}{"Gols":12}{"Total":<5}')
@@ -9,24 +10,18 @@ for c, e in enumerate(jogadores):
     print(f'{c:<5}{e["nome"]:<10}{e["gols"]}{e["total"]:>8}')
 print('-'*35)
 
+while True:
+    idx = int(input('Mostrar dados de qual jogador ? [999 para Sair] '))
+    print('-' * 30)
+    if idx == 999:
+        break
+    if idx > len(jogadores) - 1:
+        print('Erro !! Não existe esse índice...')
+        continue
+    print('-- LEVANTAMENTO DO JOGADOR:', jogadores[idx]['nome'])
+    for c, d in enumerate(jogadores[idx]['gols']):
+        print(f'   No jogo {c} fez {d} gols.')
+    print('-' * 30)
 
-'''
-for c, e in enumerate(jogadores):
-    #print(f'{c:<5}{e["nome"]:<10}{e["gols"]}')
-    for v in e.values():
-        #print(e)
-        print(v[0])
-'''
-'''
-for c, e in enumerate(jogadores):
-    for v in e.values():
-        print(f'v = {v}')
-        if type(v) == str:
-            print(v)
-            nome = v
-        if type(v) == list:
-            print(len(v))
-            for c in range(len(v)):
-                tot += v[c]
-            print(tot)
-'''
+print(f'{" Fim do programa ":=^30}')
+
