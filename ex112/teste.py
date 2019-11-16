d12253149909163e8db4be32e90cc3cd08ff4876
henrique <h.macedo@hotmail.com>
2019-11-16 19:55:04 -0300

    Arquivos atualizados às 19:54 hs de 16/11/2019

--- ex112/teste.py
+++ ex112/teste.py
@@ -1,5 +1,6 @@
-from ex111.utilidadescev import moeda
+from ex112.utilidadescev import moeda
+from ex112.utilidadescev import dado
 
-p = float(input('Digite um valor: R$ '))
+p = dado.leiaDinheiro('Digite um valor: R$ ')
 print()
 moeda.resumo(p, 35, 12)
