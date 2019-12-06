2cab8251f0056fa9389378a49f3922d60aec0b22
henrique <h.macedo@hotmail.com>
2019-12-05 23:05:21 -0300

    Adicionado arquivos referentes ao Curso Introdutório de Python da apostila baixada do link "https://readthedocs.org/projects/curso-python/downloads/pdf/latest/"

--- CursoIntroPython/Exer6.6/ex06.py
+++ CursoIntroPython/Exer6.6/ex06.py
@@ -6,10 +6,12 @@ Um pote de sorvete tem dimensões 15 cm * 10 cm * 13 cm. Uma bolinha de queijo �
 de raio r = 1.2 cm. O fator de empacotamento ideal é 0.74, mas o pote de sorvete tem tamanho
 comparável às bolinhas de queijo, aí tem efeitos de borda, então o fator deve ser menor. Mas as
 bolinhas de queijo são razoavelmente elásticas, então empacota mais. Esse valor parece razoável.
+
 Sabendo que o volume de uma esfera de raio 𝑟 é 𝑉 = 4/3 * 𝜋 * 𝑟3, o volume do pote de sorvete é 𝑉 = 𝑥 * 𝑦 * 𝑧 e o fator
 de empacotamento é a fração de volume ocupado pelas bolinhas de queijo. Ou seja, 74% do pote de sorvete vai
 ser ocupado pelas bolinhas de queijo.
 
 Ajude a Krissia descobrir quantas bolinhas de queijo cabem no pote de sorvete!
 """
+
 
