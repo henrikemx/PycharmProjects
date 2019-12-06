78366ac89daed31b487c63bcfcbc62614d4b2d81
henrique <h.macedo@hotmail.com>
2019-12-06 20:34:07 -0300

    Atualizações de arquivos referentes ao Curso Introdutório de Python da apostila baixada do link "https://readthedocs.org/projects/curso-python/downloads/pdf/latest/" às 20:34 hs de 06/12/2019.

--- CursoIntroPython/Exer7.7/ex03.py
+++ CursoIntroPython/Exer7.7/ex03.py
@@ -5,9 +5,38 @@
 # de tudo isso para a cidade de Rolândia, no Paraná, ficou em USD 12,34.
 #
 #   a. Calcule o valor total da compra em dólares.
-#   b.Usando o mesmo valor do dólar do exercício anterior, calcule
-#     o preço final em Reais. Lembre-se que o # valor do IOF é de 6,38 %.
-#   c. uanto ela pagou apenas de IOF?
+#   b. Usando o mesmo valor do dólar do exercício anterior, calcule
+#       o preço final em Reais. Lembre-se que o # valor do IOF é de 6,38 %.
+#   c. Quanto ela pagou apenas de IOF?
 #
 #####################################################################
 
+print('Itens a serem adquiridos por Josefa da China:'
+      ''''\n----------------------------------------------
+1. Celular:                        US$ 299,99
+2. Chaleira:                       US$  23,87
+3. Gnomo de Jardim:                US$  66,66
+4. 6 adesivos de Unicórnio (cada): US$   1,42
+      ''')
+print('Valor do frete:                    US$  12,34')
+
+# Declarando as variáveis e seus valores...
+cel = 299.99
+chal = 23.97
+gnomo = 66.66
+advUni = 1.42
+totadvUni = advUni * 6
+totCompra = cel + chal + gnomo + totadvUni
+cotDolar = 3.25
+
+taxaIOF = totCompra * 6.38/100
+totCompraR = (totCompra + taxaIOF) * cotDolar
+
+print('#'*50)
+print(f'O valor total da compra será de US$ {totCompra:.2f}')
+
+print(f'Valor do Dólar: R$ \033[32m{cotDolar}\033[0;0m')
+print('Valor do IOF: \033[33m6,38%\033[0;0m')
+print(f'O valor final da compra, já convertido e acrescido do IOF\n'
+      f'ficará em: R$ \033[34m\033[1m{totCompraR:.2f}\033[0;0m')
+print(f'Josefa irá pagar, só de IOF sobre essa compra, R$ \033[31m{taxaIOF:.2f}\033[0;0m')
