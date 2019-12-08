23fb86716ab9daaf2c7a7f2f8dbafb1b600f1834
henrique <h.macedo@hotmail.com>
2019-12-08 19:35:27 -0300

    Atualizado às 19:35 hs de 08/12/2019

--- CursoIntroPython/Exer7.7/calcMA.py
+++ CursoIntroPython/Exer7.7/calcMA.py
@@ -6,22 +6,64 @@
 # cício proposto na apostila para cálculo de
 # Média Aritmética, Média Geométrica e Média
 # Harmônica...
+#
+#   M.A. = (p1 + p2 + p3 + p4)/4
+#
+#   M.G. = 4V(P1 * P2 * P3 * P4)
+#
+#   M.H. = 4/(1/P1 + 1/P2 + 1/P3 + 1/P4)
 ############################################
 
 # declaração da variável
 media = []
-soma = 0
+md = []
+soma = inverso = maior = menor = 0
+mult = 1
 
 # informar os avlores das notas bimestrais...
-for c in range(0, 5):
+print('='*20)
+for c in range(1, 5):
     media.append(float(input(f'Informe a {c}ª nota: ')))
 
 # expressão para cálculo da M.A.
 # ma = (n1 + n2 + n3 + n4) / 4
-# tentando calcular usando loop for
+# calculando a M.A.
 for n in range(0, len(media)):
     soma += media[n]
 ma = soma / 4
 
+# calculando a M.G...
+for n in range(0, len(media)):
+    mult *= media[n]
+mg = mult ** (1/4)
+
+# calculando a M.H...
+for n in range(0, len(media)):
+    inverso += 1/media[n]
+mh = 4 / inverso
+
 # apresentação do resultado...
-print(f'O valor da M.A. do aluno foi de \033[33m{}\033[0;0m.')
+print('='*30)
+print(f'=> Valores obtidos de soma = {soma:.2f}, mult = {mult:.2f} e inverso = {inverso:.2f}...')
+print('='*30)
+print(f'=> O valor da M.A. do aluno foi de \033[33m{ma:.2f}\033[0;0m.')
+print(f'=> O valor da M.G. do aluno foi de \033[33m{mg:.2f}\033[0;0m.')
+print(f'=> O valor da M.H. do aluno foi de \033[33m{mh:.2f}\033[0;0m.')
+print('='*30)
+# antes de apresentar qual foi a MAIOR e MENOR média, será necessário
+# atribuit os valores a uma lista...
+md = [ma, mg, mh]
+
+# e para finalizar, será exibido qual a MAIOR e MENOR média...
+for m in range(0, len(md)):
+    if m == 0:
+        maior = md[m]
+        menor = md[m]
+    else:
+        if md[m] > maior:
+            maior = md[m]
+        else:
+            menor = md[m]
+
+print(f'=> A MAIOR média foi de \033[32m{maior:.2f}\033[0;0m e a MENOR de \033[31m{menor:.2f}\033[0;0m')
+print('='*30)
